83ecccf21038e271a8e59b66fd528eb2fd4b5541
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2022-10-18 00:57:37 +0530

    NavBar & Header Part has been done: git commit -mNavBar

---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ProPic from "../assets/Gowtham-Chokkalinga-fw20_0116.jpg";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+  return (
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div>
+          <h1>I'm An Aspiring Full Stack Web-Developer</h1>
+          <p>
+            passionate Full-Stack Developer, Dedicated to developing robust applications that interact with the Front & Backend part of web applications.
+            Extensive expertise in website architecture, design, and web design, and web API designs. Problem-solving mindset with a goal to optimize
+            my application for better stability and speed. Love to work in a team.
+          </p>
+          <div>
+            <button>
+              Portfolio
+              <span>
+                <MdKeyboardArrowRight></MdKeyboardArrowRight>
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <img className="md:w-[70%] w-[40%] md:rounded-full  duration-500 hover:scale-110 duration-500" src={ProPic}  alt="/"></img>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+const NavBar = () => {
+  const [nav, setNav] = React.useState(false);
+  const links = [
+    { id: 1, link: "home" },
+    { id: 2, link: "about" },
+    { id: 3, link: "portfolio" },
+    { id: 4, link: "experience" },
+    { id: 5, link: "contact" },
+  ];
+  return (
+    <div className="flex justify-between items-center w-full h-16 text-white bg-black px-4 fixed">
+      <div>
+        <h1 className="text-2xl font-title ml-2 hover:scale-105 hover:text-purple-400 duration-200">Gowtham</h1>
+      </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 font-title cursor-pointer capitalize font-medium text-gray-200 hover:scale-105 hover:text-purple-400 duration-200"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+
+      <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
+        {nav ? <FaTimes size={30}></FaTimes> : <FaBars size={30}></FaBars>}
+      </div>
+      {nav && (
+        <ul className="flex flex-col justify-center absolute top-0 right-0 w-60 h-screen bg-gradient-to-b from-black to-gray-800 text-gray-200">
+          {links.map(({ id, link }) => (
+            <li key={id} className="px-4 text-2xl cursor-pointer capitalize py-6 hover:text-purple-500">
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
