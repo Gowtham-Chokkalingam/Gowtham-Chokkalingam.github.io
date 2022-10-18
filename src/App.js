5cff2efecc06fac31cae13487a84d5acb057bce4
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2022-10-19 00:14:07 +0530

    Social Links Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Home></Home>
+
+      
+      <SocialLinks></SocialLinks>
     </div>
   );
 }
 
 export default App;
+
