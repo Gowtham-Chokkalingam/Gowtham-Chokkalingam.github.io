e18a95e2e7be463e3b6c2cba4b5b00706c8c836c
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2022-12-30 14:28:29 +0530

    added flipkart project

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -4,11 +4,22 @@ import myhours from "../../assets/portfolio/myhours.png";
 import foodapp from "../../assets/portfolio/foodapp.png";
 import weather from "../../assets/portfolio/weather.png";
 import zee5 from "../../assets/portfolio/zee5.png";
+import flipkart from "../../assets/portfolio/Fipkartclone.png";
 import shoping from "../../assets/portfolio/shopingCartRedux.png";
 
 import reactMovieapp from "../../assets/portfolio/MovieApp-Redux-React.png";
 
 const projects = [
+  {
+    id: 0,
+    title: "Flipkart",
+    src: flipkart,
+    link: "https://filpkart-clone-clientgc.onrender.com/",
+    git: "https://github.com/Gowtham-Chokkalingam/E-Com-Clone",
+    style: "hover:text-blue-400 duration-200",
+    disc: "A clone of the famous e-commerce website Flipkart. I have implemented various features in this project like User authentication using context API, searching products, and adding products into the cart with quantity updating buttons. I have used React to build the Front-end and MongoDB for the Back-end. & MUI used for styling",
+    techStack: ["ReactJs", "MongoDB", "NodeJS", "Redux"],
+  },
   {
     id: 0,
     title: "Zee5.com",
@@ -21,6 +32,16 @@ const projects = [
   },
 
   {
+    id: 2,
+    title: "React Movie-Searching App",
+    src: reactMovieapp,
+    link: "https://moive-app-gowtham-chokkalingam.vercel.app/",
+    git: "https://github.com/Gowtham-Chokkalingam/react-projects/tree/master/moive-app",
+    style: "hover:text-blue-400 duration-200",
+    disc: "This is Movie App built by using React, SCSS and React-Redux-Toolkit. This app will give you the information about Movies which we search and also displays the trending moives by default.",
+    techStack: ["React", "SCSS", "Redux-Toolkit", "Axios-API"],
+  },
+  {
     id: 1,
     title: "Myhours",
     src: myhours,
@@ -30,8 +51,9 @@ const projects = [
     disc: "MyHour is task and time tracking website used for tracking live project status.This was an Individual Project. In this I have created on Homepage, Login page with help of React , Chakra and Bootstrap for UI.",
     techStack: ["React", "REST API", "NodeJS", "Chakra"],
   },
+
   {
-    id: 2,
+    id: 3,
     title: "Movie Searching App",
     src: movieapp,
     link: "https://admirable-buttercream-87249d.netlify.app/",
@@ -39,16 +61,6 @@ const projects = [
     style: "hover:text-blue-400 duration-200",
     disc: "This is simple moive searching app, and by default this app will shows the trending moives. I have built this website with help of plain java script and used OMBD api for fetching the data",
     techStack: ["HTML", "CSS", "JS", "Axios-API", "NodeJS"],
-  },
-  {
-    id: 3,
-    title: "React Movie-Searching App",
-    src: reactMovieapp,
-    link: "https://moive-app-gowtham-chokkalingam.vercel.app/",
-    git: "https://github.com/Gowtham-Chokkalingam/react-projects/tree/master/moive-app",
-    style: "hover:text-blue-400 duration-200",
-    disc: "This is Movie App built by using React, SCSS and React-Redux-Toolkit. This app will give you the information about Movies which we search and also displays the trending moives by default.",
-    techStack: ["React", "SCSS", "Redux-Toolkit", "Axios-API"],
   },
   {
     id: 4,
@@ -60,7 +72,7 @@ const projects = [
     disc: "This is a food searching app, this app will shows the foods whichever you search. I have buit this app using HTML, CSS, and JavaScript. Api used to fetch the data and rendering on DOM",
     techStack: ["HTML", "CSS", "JS", "API", "NodeJS"],
   },
- 
+
   {
     id: 5,
     title: "Shoping App",
@@ -69,7 +81,7 @@ const projects = [
     git: "https://github.com/Gowtham-Chokkalingam/Shoping-Cart-React-Redux",
     style: "hover:text-blue-400 duration-200",
     disc: "This is a shoping app made with ReactJs and Redux, In this app user can add the product to the cart,able to increase and decrease the quantity as well. React-BootStrap and Material-UI is used for UI design",
-    techStack: ["React", "React-BootStrap", "Material-UI", ],
+    techStack: ["React", "React-BootStrap", "Material-UI"],
   },
   {
     id: 6,
